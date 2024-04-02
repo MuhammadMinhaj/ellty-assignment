7fd87a2f1a07f8436d37e5a2daef7d408be2e9ae
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2024-04-03 03:45:20 +0600

    feat: Implemented checkbox component with styles and stories.

---
+++ src/components/core/checkbox/Checkbox.styles.ts
@@ -0,0 +1,21 @@
+/**
+ * Defined root styles
+ */
+export const rootClasses = /*tw*/ 'relative';
+
+/**
+ * Defined label styles
+ */
+export const labelClasses =
+  /*tw*/ 'absolute top-0 left-0 bg-white cursor-pointer w-[25px] h-[25px] flex items-center justify-center [&>*]:opacity-0 border-[1px] border-border-gray-main hover:border-border-gray-deep hover:text-border-gray-light hover:[&>*]:opacity-100';
+
+/**
+ * Defined input classes
+ * */
+export const inputClasses =
+  /*tw*/ 'opacity-0 [&:checked+label>*]:opacity-100 [&:checked+label]:bg-accent [&:checked+label]:border-0 [&:checked+label]:text-white [&:checked+label]:after:opacity-100 [&:checked:hover+label]:bg-accent-light [&:checked:hover+label]:shadow-[0px_0px_0px_3px_rgba(36,105,246,0.10)] [&:checked:active+label]:!bg-transparent [&:checked:active+label]:!shadow-none [&:checked:active+label]:!border-[1px]  [&:checked:active+label]:text-border-gray-light [&:active+label]:border-border-gray-deep [&:active+label]:text-text-gray [&:active+label>*]:opacity-100 [&:active+label]:shadow-[0px_0px_0px_3px_rgba(36,105,246,0.10)]';
+
+/**
+ * Rounded classes
+ */
+export const roundedClass = 'rounded-[6px]';
---
+++ src/components/core/checkbox/Checkbox.tsx
@@ -0,0 +1,60 @@
+import clsx from 'clsx';
+import { FC, InputHTMLAttributes } from 'react';
+import { inputClasses, labelClasses, rootClasses, roundedClass } from './Checkbox.styles';
+
+// Checkbox Props
+type CheckboxProps = InputHTMLAttributes<HTMLInputElement> & {
+  rounded?: boolean;
+  rootClassName?: string;
+  labelClassName?: string;
+};
+
+// Checkbox
+const Checkbox: FC<CheckboxProps> = ({
+  className,
+  rootClassName,
+  labelClassName,
+  rounded = true,
+  id = 'checkbox',
+  ...rest
+}) => {
+  /**
+   * Parent classes
+   */
+  const combinedRootClasses = /*tw*/ clsx(rootClasses, rootClassName);
+
+  /**
+   * Label classes
+   * */
+  const combinedLabelClasses = /*tw*/ clsx(labelClasses, labelClassName, {
+    [roundedClass]: rounded
+  });
+
+  /**
+   * Input classes
+   * */
+  const combinedInputClasses = /*tw*/ clsx(inputClasses, className);
+
+  return (
+    <div className={combinedRootClasses}>
+      <input type="checkbox" id={id} className={combinedInputClasses} {...rest} />
+      <label htmlFor={id} className={combinedLabelClasses}>
+        <svg
+          width="19"
+          height="14"
+          viewBox="0 0 19 14"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M1 7.6L7.03451 12.9672C7.05497 12.9854 7.08626 12.9837 7.1047 12.9635L18 1"
+            stroke="currentColor"
+            stroke-linecap="round"
+          />
+        </svg>
+      </label>
+    </div>
+  );
+};
+
+export default Checkbox;
