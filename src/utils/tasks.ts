1c2c8d3cb8a88d38c8a1fc29aca61920cf7cec81
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2024-04-04 01:12:51 +0600

    feat: added TaskType type and Task data

---
+++ src/utils/tasks.ts
@@ -0,0 +1,34 @@
+import { TaskType } from './types';
+
+export const tasksList: TaskType[] = [
+  {
+    id: 'task-1',
+    name: 'Planning',
+    isCompleted: false
+  },
+  {
+    id: 'task-2',
+    name: 'Requirements',
+    isCompleted: false
+  },
+  {
+    id: 'task-3',
+    name: 'Design and prototype',
+    isCompleted: false
+  },
+  {
+    id: 'task-4',
+    name: 'Software Development',
+    isCompleted: false
+  },
+  {
+    id: 'task-5',
+    name: 'Testing',
+    isCompleted: false
+  },
+  {
+    id: 'task-6',
+    name: 'Deployment & Maintenance',
+    isCompleted: false
+  }
+];
