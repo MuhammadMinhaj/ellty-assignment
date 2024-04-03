1ca0b49d8adff5d9934c1ea45643edb6912a5897
Muhammad Minhaj <developer.mdminhaj@gmail.com>
2024-04-04 01:06:16 +0600

    chore: included utils to webpack alias resolver

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -18,6 +18,7 @@ const config: StorybookConfig = {
       "@/components": path.resolve(__dirname, "../src/components"),
       "@/core": path.resolve(__dirname, "../src/components/core"),
       "@/features": path.resolve(__dirname, "../src/components/features"),
+      "@/utils": path.resolve(__dirname, "../src/utils"),
     };
     return config
   },
